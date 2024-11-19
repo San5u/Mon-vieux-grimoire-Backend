e6131944475f33cfab9179a0f947da9c00363073
San5u <thomaslobjois@ymail.com>
2024-11-19 11:35:31 +0100

    delete

--- middleware/sharp.js
+++ middleware/sharp.js
@@ -11,12 +11,13 @@ const compressImage = async (req, res, next) => {
     const outPath = path.join(__dirname, "../images", filename);
     try {
         await sharp(req.file.buffer)
-        .webp({ quality: 80 })
+        .webp({ quality: 60 })
         .toFile(outPath);
+        console.log(req)
         req.file.filename = filename; //MAJ du nom du fichier
         next();
     } catch (error) {
-        res.status(500).json({ error: "Erreur lors du traitement de l'image" });
+        res.status(500).json({ error: "Erreur lors du traitement de l'image", req:name, req1:filename, req2:outPath, req3:error});
     }
 };
 
